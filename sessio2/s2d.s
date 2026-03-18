05aa8f9eef6609f6a5ce8ce96e3768c86b640b09
San-tito <68120901+San-tito@users.noreply.github.com>
2026-03-18 21:14:57 +0100

    s2d fet

--- sessio2/s2d.s
+++ sessio2/s2d.s
@@ -9,70 +9,65 @@ count:   .word 0
 .text
 .globl main
 main:
-   addiu $sp, $sp, -4
-   sw    $ra, 0($sp) 
-   la    $s0, count
-   la    $a0, w1
-   la    $a1, w2
-   jal   codifica 
-   sw    $v0, 0($s0)
-   la    $a0, w2
-   la    $a1, w3
-   jal   codifica 
-   lw    $s1, 0($s0)
-   addu  $s1, $s1, $v0
-   sw    $s1, 0($s0)
-   li    $v0, 4
-   la    $a0, w2
-   syscall
-   la    $a0, w3
-   syscall
-   lw    $ra, 0($sp)
-   addiu $sp, $sp, 4
-   jr    $ra
+   addiu $sp, $sp, -4       # reserve stack
+   sw    $ra, 0($sp)        # save ra
+   la    $s0, count         # s0 = @count
+   la    $a0, w1            # a0 = @w1
+   la    $a1, w2            # a1 = @w2
+   jal   codifica           # v0 = codifica(w1, w2)
+   sw    $v0, 0($s0)        # count = v0
+   la    $a0, w2            # a0 = @w2
+   la    $a1, w3            # a1 = @w3
+   jal   codifica           # v0 = codifica(w2, w3)
+   lw    $s1, 0($s0)        # s1 = count
+   addu  $s1, $s1, $v0      # s1 = count + v0
+   sw    $s1, 0($s0)        # count = s1
+   li    $v0, 4             # v0 = 4 (print_string)
+   la    $a0, w2            # a0 = @w2
+   syscall                  # print_string(w2)
+   la    $a0, w3            # a0 = @w3
+   syscall                  # print_string(w3)
+   lw    $ra, 0($sp)        # restore ra
+   addiu $sp, $sp, 4        # release stack
+   jr    $ra                # return
 
-g:  
-   lb    $t0, 0($a1)
-   li    $t1, 'A'
-   addiu $t1, $t1, 25
-   sub   $t1, $t1, $t0
-   add   $t0, $a0, $t1
-   lb    $v0, 0($t0) 
-  
-   jr $ra
+g:
+   lb    $t0, 0($a1)        # t0 = *char
+   li    $t1, 'A'           # t1 = 'A'
+   addiu $t1, $t1, 25       # t1 = 'Z'
+   sub   $t1, $t1, $t0      # t1 = 'Z' - *char
+   add   $t0, $a0, $t1      # t0 = @alfabet + offset
+   lb    $v0, 0($t0)        # v0 = alfabet[offset]
+
+   jr $ra                   # return
 
 
 codifica:
-   addiu $sp, $sp, -16
-   sw    $ra, 0($sp)
-   sw    $s0, 4($sp)
-   sw    $s1, 8($sp)
-   sw    $s2, 12($sp)
-   move  $s2, $zero
-   move  $s0, $a0
-   move  $s1, $a1 
+   addiu $sp, $sp, -16      # reserve stack
+   sw    $ra, 0($sp)        # save ra
+   sw    $s0, 4($sp)        # save s0
+   sw    $s1, 8($sp)        # save s1
+   sw    $s2, 12($sp)       # save s2
+   move  $s2, $zero         # s2 = i = 0
+   move  $s0, $a0           # s0 = @src
+   move  $s1, $a1           # s1 = @dest
 while:
-   lb    $t0, 0($s0)
-   lb    $t0, 0($t0)
-   beq   $t0, $zero, fi_while
-   la    $a0, alfabet
-   move  $a1, $s1
-   jal   g  
-   sb    $v0, 0($s0)  
-   addiu $s0, $s0, 1
-   addiu $s1, $s1, 1
-   addiu $s2, $s2, 1  
-   b     while     
+   lb    $t0, 0($s0)        # t0 = src[i]
+   beq   $t0, $zero, fi_while # if (t0 == '\0') goto fi_while
+   la    $a0, alfabet       # a0 = @alfabet
+   move  $a1, $s0           # a1 = @src[i]
+   jal   g                  # v0 = g(alfabet, &src[i])
+   sb    $v0, 0($s1)        # dest[i] = v0
+   addiu $s0, $s0, 1        # src++
+   addiu $s1, $s1, 1        # dest++
+   addiu $s2, $s2, 1        # i++
+   b     while              # goto while
  fi_while:
-   sb    $zero, 0($s1)
-   move  $v0, $s2
-   lw    $ra, 0($sp)
-   lw    $s0, 4($sp)
-   lw    $s1, 8($sp)
-   lw    $s2, 12($sp)
-   addiu $sp, $sp, 16
-   jr    $ra
-
-
-
- 
+   sb    $zero, 0($s1)      # dest[i] = '\0'
+   move  $v0, $s2           # return i
+   lw    $ra, 0($sp)        # restore ra
+   lw    $s0, 4($sp)        # restore s0
+   lw    $s1, 8($sp)        # restore s1
+   lw    $s2, 12($sp)       # restore s2
+   addiu $sp, $sp, 16       # release stack
+   jr    $ra                # return
