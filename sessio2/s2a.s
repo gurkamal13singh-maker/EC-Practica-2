d22dff04875100588341e0250e746b3658172864
San-tito <68120901+San-tito@users.noreply.github.com>
2026-03-18 17:16:52 +0100

    add comments and refactor

--- sessio2/s2a.s
+++ sessio2/s2a.s
@@ -1,19 +1,19 @@
-	.data
+.data
 
-	.text
-	.globl main
+    .text
+    .globl main
 
 main:
-	li $s1, 23 # Y
-	li $s0, 8  # X
+    li $s1, 23              # s1 = Y
+    li $s0, 8               # s0 = X
 
-	addiu $t0, $zero, 1     # $t0 = 1
-	sllv  $t0, $t0, $s0     # $t0 = (1 << X)
-	addiu $t0, $t0, -1      # $t0 = (1 << X) - 1
-	xor   $s1, $s1, $t0     # $s1 = Y ^ ((1 << X) - 1)
+    addiu $t0, $zero, 1     # t0 = 1
+    sllv  $t0, $t0, $s0     # t0 = (1 << X)
+    addiu $t0, $t0, -1      # t0 = (1 << X) - 1
+    xor   $s1, $s1, $t0     # s1 = Y ^ ((1 << X) - 1)
 
-	addu  $a0, $zero, $s1   # $a0 = $s1
-	addiu $v0, $zero, 1     # $v0 = 1 (print_int)
-	syscall                 # print_int($s1)
+    addu  $a0, $zero, $s1   # a0 = s1
+    addiu $v0, $zero, 1     # v0 = 1 (print_int)
+    syscall                 # print_int(s1)
 
-	jr $ra
+    jr $ra                  # return
--- sessio2/s2b.s
+++ sessio2/s2b.s
@@ -1,52 +1,53 @@
-		.data
+.data
 result: .word 0
-num:	.byte '7'
+num:    .byte '7'
 
-	.text
-	.globl main
+    .text
+    .globl main
 main:
 
-# COPIA EL TEU CODI AQUI
-
-	la $t0, num
-	lb $t0, 0($t0)   # $t0 <- num
+    la $t0, num
+    lb $t0, 0($t0)      # t0 = num
 
 if:
-	li $t1, 'a'
-	blt $t0, $t1, cond2
-	li $t1, 'z'
-	ble $t0, $t0, exec
-	
+    li $t1, 'a'
+    blt $t0, $t1, cond2 # if (t0 < 'a') goto cond2
+    li $t1, 'z'
+    bgt $t0, $t1, cond2 # if (t0 > 'z') goto cond2
+    
 cond2: 
-	li $t1, 'A'
-	blt $t0, $t1, else
-	li $t1, 'Z'
-	bgt $t0, $t0, else
-	
+    li $t1, 'A'
+    blt $t0, $t1, else  # if (t0 < 'A') goto else
+    li $t1, 'Z'
+    bgt $t0, $t1, else  # if (t0 > 'Z') goto else
+    
 exec:
-	la $t1, result
-	sw $t0, 0($t1)   # result = num
-	j fi
-	
+    la $t1, result
+    sw $t0, 0($t1)      # result = t0
+    j fi
+    
 else: 
-	li $t1, '0'
-	blt $t0, $t1, else2
-	li $t1, '9'
-	bgt $t0, $t1, else2
-	
+    li $t1, '0'
+    blt $t0, $t1, else2 # if (t0 < '0') goto else2
+    li $t1, '9'
+    bgt $t0, $t1, else2 # if (t0 > '9') goto else2
+    
 exec2:
-	li $t1, '0'
-	sub $t1, $t0, $t1   # $t1 = num - '0'
-	la $t2, result
-	sw $t1, 0($t2)      # result = num
-	j fi
+    li $t1, '0'
+    sub $t1, $t0, $t1   # t1 = t0 - '0'
+    la $t2, result
+    sw $t1, 0($t2)      # result = t1
+    j fi
 
 else2:
-	li $t1, -1     # t1 = -1
-	la $t2, result    #t2 = @result
-	sw $t1, 0($t2)      #result = -1
+    li $t1, -1          # t1 = -1
+    la $t2, result      
+    sw $t1, 0($t2)      # result = -1
 
 fi:
-	jr $ra
+    la $t0, result
+    lw $a0, 0($t0)      # a0 = result
+    addiu $v0, $zero, 1 # v0 = 1 (print_int)
+    syscall             # print_int(result)
 
-
+    jr $ra              # return
